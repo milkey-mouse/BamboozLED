c0c15e5efed2b43f9c377968d362f0391559a969
Milkey Mouse <milkeymouse@meme.institute>
2017-04-16 21:11:14 -0700

    Add Python test script
    This imports a modified opc.py from https://github.com/zestyping/openpixelcontrol

---
+++ scan-pixels.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from itertools import chain
+import time
+import opc
+
+client = opc.Client("localhost:7891")
+if client.can_connect():
+    for i in chain(range(5), range(3, -1, -1)):
+        arr = [(0, 0, 0, 255)] * 5
+        arr[i] = (255, 255, 255, 255)
+        client.put_pixels(arr, channel=1)
+        time.sleep(0.25)
+else:
+    print("Can't connect to server")
